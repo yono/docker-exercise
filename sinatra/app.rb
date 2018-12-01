3316bfb559c48df7df15b65364bff04b9e673a56
Yusaku ONO <yono.05@gmail.com>
2018-12-01 18:02:47 +0900

    work when redis is not running

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,20 +1,26 @@
 require 'sinatra'
 require 'redis'
 
-redis = Redis.new host: ENV['REDIS_PORT_6379_TCP_ADDR'], port: ENV['REDIS_PORT_6379_TCP_PORT']
+if ENV['REDIS_PORT_6379_TCP_ADDR']
+  redis = Redis.new host: ENV['REDIS_PORT_6379_TCP_ADDR'], port: ENV['REDIS_PORT_6379_TCP_PORT']
 
-get '/' do
-  @messages = redis.lrange :messages, 0, 9
-  erb :home
-end
+  get '/' do
+    @messages = redis.lrange :messages, 0, 9
+    erb :home
+  end
 
-post '/messages' do
-  message = params[:message]
-  redis.lpush :messages, message
-  redirect '/'
-end
+  post '/messages' do
+    message = params[:message]
+    redis.lpush :messages, message
+    redirect '/'
+  end
 
-delete '/messages' do
-  redis.del :messages
-  redirect '/'
+  delete '/messages' do
+    redis.del :messages
+    redirect '/'
+  end
+else
+  get '/' do
+    'Hello Docker World!'
+  end
 end
